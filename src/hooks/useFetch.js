28f26bbb1a2a4c9a9c886fcd28467e516f88d09a
findvinu <vinodyadavhhh4@gmail.com>
2024-08-23 13:22:31 +0530

    api update

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_NEWS_API_URL;
+const BASE_URL = import.meta.env.VITE_NEWS_API_URL || "https://gnews.io/api/v4";
 
 const useFetch = ({
+  category = "general",
+  lang = "en",
   country = "us",
-  apiKey = "8f980c9f4adf42a4bd1820978cf92ad4",
+  max = 10,
+  apiKey = "a7c3ff7320f98c1bbd200eb4237b0751",
 }) => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -15,13 +18,19 @@ const useFetch = ({
     const fetchData = async () => {
       setLoading(true);
       try {
-        const response = await axios.get(
-          `${BASE_URL}?country=${country}&apiKey=${apiKey}`
-        );
+        const response = await axios.get(`${BASE_URL}/top-headlines`, {
+          params: {
+            category,
+            lang,
+            country,
+            max,
+            apikey: apiKey,
+          },
+        });
         console.log("Success:", response.data.articles);
-
-        setData((prevData) => [...prevData, ...response.data.articles]);
+        setData(response.data.articles);
       } catch (err) {
+        console.error("Error fetching data:", err);
         setError(err.message || "API is not available.");
       } finally {
         setLoading(false);
@@ -29,7 +38,7 @@ const useFetch = ({
     };
 
     fetchData();
-  }, [country, apiKey]);
+  }, [category, lang, country, max, apiKey]);
 
   return { data, loading, error };
 };
